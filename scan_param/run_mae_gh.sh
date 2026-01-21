8090a7d42ac284aa897b1cb2523dd811f09b58e3
Weiyuan Li <weiyuanli-icepp@users.noreply.github.com>
2026-01-21 14:03:53 +0100

    Scan MAE mask ratios on GH

--- scan_param/run_mae_gh.sh
+++ scan_param/run_mae_gh.sh
@@ -4,7 +4,6 @@
 export EPOCHS=50
 # export BATCH=1024
 # export CHUNK_SIZE=256000
-export MASK_RATIO=0.65  # 75% masking is standard for MAE
 export RESUME_FROM=""
 # export PARTITION="a100-daily"
 export PARTITION="gh-daily"
@@ -14,7 +13,7 @@ export CONFIG_PATH="config/mae_config.yaml"
 # export NPHO_SCALE2="1.0"
 export TIME_SCALE="6.5e-8"
 export TIME_SHIFT="0.5"
-# export SENTINEL_VALUE="-5.0"
+export SENTINEL_VALUE="-3.0"
 export LOSS_FN="smooth_l1"
 export TIME_WEIGHT="0.05"
 export AUTO_WEIGHT="false"
@@ -26,6 +25,15 @@ export LR_MIN="1e-8"
 export MLFLOW_EXPERIMENT="mae_pretraining"
 
 # export RUN_NAME="mae_tw0.05_tsc6.5e-8_tsh0.5_lr2.5e-4_lrmin1e-8_epochs50_mask0.2_cosine"
-export RUN_NAME="mae_mask0.60"
+# MASK_RATIOS=(0.60 0.65 0.70 0.75)
+MASK_RATIOS=(0.65)
+SENTINEL_VALUES=( -5.0 -3.0 -1.0 )
 
-./submit_mae.sh
+for MASK_RATIO in "${MASK_RATIOS[@]}"; do
+    for SENTINEL_VALUE in "${SENTINEL_VALUES[@]}"; do
+        export MASK_RATIO
+        export SENTINEL_VALUE
+        export RUN_NAME="mae_mask${MASK_RATIO}_sentinel${SENTINEL_VALUE}"
+        ./submit_mae.sh
+    done
+done
