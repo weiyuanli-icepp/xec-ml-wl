764ae0a2c914a6ef374a2ac00a78143fb744d4ff
weiyuanli-icepp <weiyuan@icepp.s.u-tokyo.ac.jp>
2026-02-25 11:09:28 +0100

    Reduce mask_ratio to 0.10 to match realistic dead channel density

--- jobs/run_mae.sh
+++ jobs/run_mae.sh
@@ -5,7 +5,7 @@
 # Inpainter training with MAE pretrained encoder (frozen).
 # All settings are in the YAML config; this script just sets job-level overrides.
 
-export CONFIG_PATH="config/inp/inp_mask0.20_mae_frozen.yaml"
+export CONFIG_PATH="config/inp/inp_mask0.10_mae_frozen.yaml"
 export PARTITION="a100-daily"
 export TIME="12:00:00"
 
